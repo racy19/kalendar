4e0be32af2228f7983fec3103919b13cc7ec5688
racy19 <anna.raichlova@gmail.com>
2025-08-01 22:19:44 +0200

    set server url to fetches

--- client/src/features/auth/Login.tsx
+++ client/src/features/auth/Login.tsx
@@ -30,7 +30,7 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -76,7 +76,7 @@ const Login = () => {
 
     const handleGoogleLogin = async (googleToken: string) => {
         try {
-          const res = await fetch("http://localhost:5000/api/auth/google-login", {
+          const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/google-login`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
